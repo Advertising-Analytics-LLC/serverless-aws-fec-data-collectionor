b2d3856cf7c4e33cd373eb7413fcf6834b00af44
Steven Black <genseb7@gmail.com>
2020-07-20 11:37:42 -0400

    clarify messages, break up func, become confused

--- src/FinancialSummaryLoader.py
+++ src/FinancialSummaryLoader.py
@@ -11,8 +11,8 @@ import boto3
 import json
 import os
 from requests import Response
-from time import time
-from typing import Dict
+from time import asctime, gmtime, time
+from typing import Any, Dict, List
 from src import JSONType, logger
 from src.OpenFec import OpenFec
 from src.secrets import get_param_value_by_name
@@ -23,39 +23,86 @@ from src.sqs import pull_message_from_sqs
 API_KEY = get_param_value_by_name(os.environ['API_KEY'])
 
 # BUSYNESS LOGIC
-openFec = OpenFec(API_KEY)
 
+def parse_message(message: JSONType) -> Dict[str, str]:
+    """parse string from sqs - it's kind of jsonish
 
-def parse_message(message: Response) -> Dict[str, str]:
+    Args:
+        message (JSON): almost json
+
+    Returns:
+        Dict[str, str]: python dictionary
+    """
+
     msg_body = json.loads(message.body)
     body_content = json.loads(msg_body['Message'].replace("'", '"'))
+
     return body_content
 
-def load_reports_and_totals(committee_id):
+
+def get_filings(filters: Dict[str, str]) -> Dict[str, Any]:
+    """pulls filings from openfec api
+
+    Args:
+        filters (Dict[str, str]): filters for API query
+
+    Returns:
+        Dict[str, Any]: response as a dict
+    """
+
+    path = '/filings/'
+    reports = []
+    openFec = OpenFec(API_KEY)
+    for report in  openFec.get_route_paginator(path, filters):
+        results = report['results']
+        if results:
+            reports.append(results)
+
+    return reports
+
+
+def get_totals(committee_id: str, filters: Dict[str, Any]) -> Dict[str, Any]:
+    """[summary]
+
+    Args:
+        committee_id (str): [description]
+        filters (Dict[str, Any]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+
+    totals_path = '/committee/' + committee_id + '/totals/'
+    totals = []
+    openFec = OpenFec(API_KEY)
+    for report in openFec.get_route_paginator(totals_path, filters):
+        results = report['results']
+        if results:
+            totals.append(results)
+
+    return totals
+
+def get_filings_and_totals(committee_id: str) -> List[Dict[str, Any]]:
+    """gets filings and totals for committee
+
+    Args:
+        committee_id (str): ID of committee. eg C01234567
+
+    Returns:
+        List[Dict[str, Any]]: List of responses
+    """
     filters = {
         'committee_id': committee_id,
-        'cycle': '2020', #fallback_cycle if cycle_out_of_range else cycle,
+        'cycle': '2020', #fallback_cycle if cycle_out_of_range else cycle, # TODO: ?
         'per_page': 1,
         'sort_hide_null': True,
         'most_recent': True,
         'form_category': 'REPORT'
     }
+    filings = get_filings(filters)
+    totals = get_totals(committee_id, filters)
+    return [filings, totals]
 
-    # (3) call /filings? under tag:filings
-    # get reports from filings endpoint filter by form_category=REPORT
-    path = '/filings/'
-    reports = []
-    for report in  openFec.get_route_paginator(path, filters):
-        reports.append(report)
-
-    # (4)call committee/{committee_id}/totals? under tag:financial
-    # get financial totals
-    path = '/committee/' + committee_id + '/totals/'
-    totals = []
-    for report in openFec.get_route_paginator(path, filters):
-        totals.append(report)
-
-    return reports, totals
 
 def lambdaHandler(event: dict, context: object) -> bool:
     """see https://docs.aws.amazon.com/lambda/latest/dg/python-handler.html
@@ -69,8 +116,9 @@ def lambdaHandler(event: dict, context: object) -> bool:
     """
 
     start_time = time()
-    time_to_end = start_time + 60 * 10
-    logger.debug(f'running {__file__} from now until {time_to_end}')
+    minutes_to_run = 10
+    time_to_end = start_time + 60 * minutes_to_run
+    logger.debug(f'running {__file__} for {minutes_to_run}, from now until {asctime(gmtime(time_to_end))}')
 
     while time() < time_to_end:
         message = pull_message_from_sqs()
@@ -78,15 +126,16 @@ def lambdaHandler(event: dict, context: object) -> bool:
             return
         message_parsed = parse_message(message)
         committee_id = message_parsed['committee_id']
-        committee_data = load_reports_and_totals(committee_id)
+        committee_data = get_filings_and_totals(committee_id)
 
         if not committee_data:
             logger.error(f'Committee {committee_id} not found! exiting.')
             return False
 
         logger.debug(committee_data)
+        exit(0)
         # write_committee_data(committee_data[0])
-        # message.delete()
+        message.delete()
 
     if time() > time_to_end:
         minutes_ran = (time() - start_time) / 60

6bd11ff0dca2ebbb208aabcc606ed4d05cdcbc91
Steven Black <genseb7@gmail.com>
2020-07-21 13:49:39 -0400

    lambdas running correctly

--- src/FinancialSummaryLoader.py
+++ src/FinancialSummaryLoader.py
@@ -18,48 +18,13 @@ from src import JSONType, logger, schema
 from src.database import Database
 from src.OpenFec import OpenFec
 from src.secrets import get_param_value_by_name
+from src.sqs import delete_message_from_sqs, parse_message
 
 
 # SSM VARS
 API_KEY = get_param_value_by_name(os.environ['API_KEY'])
 
 # BUSYNESS LOGIC
-
-def parse_events(event: JSONType) -> List[Dict[str, Any]]:
-    """takes the raw event from AWS Lambda and parses into a list of messages
-
-    Args:
-        event (JSONType): List of SQS Message events
-            see https://docs.aws.amazon.com/lambda/latest/dg/with-sqs.html
-
-    Returns:
-        List[Dict[str, Any]]: List of message bodies
-    """
-
-    message_body_list = []
-    records = event['Records']
-    for record in records:
-        my_body = record['body']
-        json_body = json.loads(my_body)
-        message_body_list.append(json_body)
-
-    return message_body_list
-
-
-def parse_message(message: JSONType) -> Dict[str, str]:
-    """parse string from sqs - it's kind of jsonish
-
-    Args:
-        message (JSON): almost json
-
-    Returns:
-        Dict[str, str]: python dictionary
-    """
-
-    body_content = json.loads(message['Message'].replace("'", '"'))
-
-    return body_content
-
 
 def get_filings(filters: Dict[str, str]) -> Dict[str, Any]:
     """pulls filings from openfec api
@@ -214,8 +179,9 @@ def lambdaHandler(event:dict, context: object) -> bool:
     """
 
     logger.debug(f'running {__file__}')
+    logger.debug(event)
 
-    messages = parse_events(event)
+    messages = event['Records']
 
     for message in messages:
 
@@ -231,5 +197,7 @@ def lambdaHandler(event:dict, context: object) -> bool:
         # filing is list of lists, flatten it
         totals_flat = [item for sublist in totals for item in sublist]
         upsert_committee_totals(totals_flat)
+
+        delete_message_from_sqs(message)
 
     return True
--- src/sqs.py
+++ src/sqs.py
@@ -7,16 +7,16 @@ sqs:
 import boto3
 import os
 import json
-from src import logger
+from src import logger, JSONType
 from time import time
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 
 sqs = boto3.resource('sqs')
 SQS_QUEUE_NAME = os.environ['SQS_QUEUE_NAME']
 logger.debug(f'using queue: {SQS_QUEUE_NAME}')
-# SQS_QUEUE_NAME = os.getenv('SQS_QUEUE_NAME', '')
 queue = sqs.get_queue_by_name(QueueName=SQS_QUEUE_NAME)
+
 
 def pull_message_from_sqs() -> Dict[str, Any]:
     """pulls a committee id from SQS
@@ -31,6 +31,57 @@ def pull_message_from_sqs() -> Dict[str, Any]:
     while time() < time_to_end:
         message = queue.receive_messages(MaxNumberOfMessages=1)
         if message:
+
             return message[0]
+
     logger.info('No messages received, exiting')
+
     return {}
+
+
+def delete_message_from_sqs(message: JSONType) -> bool:
+    """deletes a message from SQS
+
+    Args:
+        message (JSONType): the SQS Message object
+
+    Returns:
+        bool: if deleted
+    """
+
+    receipt_handle = message['receiptHandle']
+    msg_id = message['messageId']
+
+    response = queue.delete_messages(
+        Entries=[{
+            'Id': msg_id,
+            'ReceiptHandle': receipt_handle
+        }])
+
+    if len(response['Successful']) > 0:
+        return True
+
+    return False
+
+
+def parse_message(message: JSONType) -> Dict[str, str]:
+    """parse string from sqs - it's kind of jsonish
+        those coming from SNS -> SQS are JSON in a 'Message' object
+        those coming from -> SQS are a string
+
+    Args:
+        message (JSON): almost json
+
+    Returns:
+        Dict[str, str]: python dictionary
+    """
+
+    my_body = message['body']
+    json_body = json.loads(my_body)
+
+    if type(json_body) == dict:
+        body_content = json.loads(json_body['Message'].replace("'", '"'))
+
+        return body_content
+
+    return json_body.replace("'", '')

ccdd675269415ef4ae10d9672fe57a1ac5f958c9
Steven Black <genseb7@gmail.com>
2020-07-27 15:17:07 -0400

    initial template

---
+++ src/CandidateSync.py
@@ -0,0 +1,60 @@
+#!/bin/env python3
+"""
+CandidateSync lambda:
+"""
+
+import json
+import os
+from datetime import datetime, timedelta
+from typing import Any, Dict, List
+from src import JSONType, logger, schema
+from src.OpenFec import OpenFec
+from src.sqs import delete_message_from_sqs, parse_message
+
+
+API_KEY = get_param_value_by_name(os.environ['API_KEY'])
+SQS_QUEUE_NAME = os.getenv('SQS_QUEUE_NAME', '')
+MIN_FIST_FILE_DATE = os.getenv('MIN_FIST_FILE_DATE', datetime.strftime(datetime.now() - timedelta(1), '%Y-%m-%d'))
+
+
+# business logic
+
+
+def get_candidates_since(isodate: str) -> json:
+    """gets list of candidates that have filed today
+
+    Args:
+        isodate: date in isoformat YYYY-MM-DD
+
+    Returns:
+        json: json list containing IDs
+    """
+
+    get_candidates_payload = {'min_last_f1_date': isodate}
+    openFec = OpenFec(API_KEY)
+
+    response_generator = openFec.get_route_paginator(
+                                '/candidates/',
+                                get_candidates_payload)
+
+    results_json = []
+    for response in response_generator:
+        results_json += response['results']
+
+    return results_json
+
+
+def lambdaHandler(event:dict, context: object) -> bool:
+    """see https://docs.aws.amazon.com/lambda/latest/dg/python-handler.html
+
+    Args:
+        event (dict): for event types see https://docs.aws.amazon.com/lambda/latest/dg/lambda-services.html
+        context (bootstrap.LambdaContext): see https://docs.aws.amazon.com/lambda/latest/dg/python-context.html
+
+    Returns:
+        bool: Did this go well?
+    """
+
+    logger.debug(f'running {__file__}')
+
+    return True

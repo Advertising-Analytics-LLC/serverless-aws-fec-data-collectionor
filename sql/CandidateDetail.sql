53dfa5417800107961cc58009b05b2eaa8b54716
Steven Black <genseb7@gmail.com>
2020-07-27 10:54:33 -0400

    fix type errors

--- sql/CandidateDetail.sql
+++ sql/CandidateDetail.sql
@@ -5,7 +5,7 @@
 
 SET search_path TO 'fec';
 
-CREATE TABLE IF NOT EXISTS candidate_detail (
+CREATE TABLE IF NOT EXISTS fec.candidate_detail (
   active_through INT DEFAULT NULL,
   address_city VARCHAR(100) DEFAULT NULL,
   address_state VARCHAR(2) DEFAULT NULL,
@@ -13,17 +13,17 @@ CREATE TABLE IF NOT EXISTS candidate_detail (
   address_street_2 VARCHAR(200) DEFAULT NULL,
   address_zip VARCHAR(10) DEFAULT NULL,
   candidate_id TEXT DEFAULT NULL,
-  candidate_inactive TINYINT(1) DEFAULT NULL,
+  candidate_inactive BOOL DEFAULT NULL,
   candidate_status VARCHAR(1) DEFAULT NULL,
-  cycles JSON DEFAULT NULL,
+  cycles TEXT DEFAULT NULL,
   district VARCHAR(2) DEFAULT NULL,
   district_number INT DEFAULT NULL,
-  election_districts JSON DEFAULT NULL,
-  election_years JSON DEFAULT NULL,
-  federal_funds_flag TINYINT(1) DEFAULT NULL,
+  election_districts TEXT DEFAULT NULL,
+  election_years TEXT DEFAULT NULL,
+  federal_funds_flag BOOL DEFAULT NULL,
   first_file_date DATE DEFAULT NULL,
   flags TEXT DEFAULT NULL,
-  has_raised_funds TINYINT(1) DEFAULT NULL,
+  has_raised_funds BOOL DEFAULT NULL,
   incumbent_challenge VARCHAR(1) DEFAULT NULL,
   incumbent_challenge_full VARCHAR(10) DEFAULT NULL,
   last_f2_date DATE DEFAULT NULL,

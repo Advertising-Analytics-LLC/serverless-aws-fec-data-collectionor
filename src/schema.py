323e2d8345ad06739f056d875fd31f90aced6ec7
Steven B <genseb7@gmail.com>
2020-07-28 14:16:19 -0400

    Create candidate loader (#19)
    * initial lambda
    
    * testing, mostly implemented
    
    * I can't spell gud
    
    * remove extra debugging
    
    * data munching
    
    * add moar spaces
    
    * account for not strings

--- src/schema.py
+++ src/schema.py
@@ -463,3 +463,71 @@ def schedule_b_update(fec_file_id: str, filing: Dict[str, Any]) -> SQL:
         .format(*[Literal(val) for key, val in values.items()], Literal(primary_key))
 
     return query
+
+
+#
+# Candidates
+#
+
+def candidate_exists(candidate_id: str) -> SQL:
+    """returns a query to check if a candidate id has a record
+
+    Args:
+        candidate_id (str): ID representing candidate
+
+    Returns:
+        SQL: select query for record
+    """
+
+    query = sql.SQL('SELECT * FROM fec.candidate_detail WHERE candidate_id={}')\
+                .format(Literal(candidate_id))
+
+    return query
+
+
+def candidate_insert(candidate: Dict[str, str]) -> SQL:
+    """inserts a record into fec.candidate_details
+
+    Args:
+        candidate (Dict[str, Any]): dictionary containing candidate data
+
+    Returns:
+        SQL: SQL insert query
+    """
+
+    values = OrderedDict(sorted(candidate.items()))
+
+    query_string = 'INSERT INTO fec.candidate_detail ('\
+        + ', '.join([f'{key}' for key, val in values.items()])\
+        + ') '\
+        + 'VALUES ('\
+        + ', '.join(['{}' for key, val in values.items()])\
+        + ')'
+
+    query = sql.SQL(query_string)\
+                .format(*[Literal(val) for key, val in values.items()])
+
+    return query
+
+
+def candidate_update(candidate: Dict[str, str]) -> SQL:
+    """updates a record in fec.candidate_detail
+
+    Args:
+        candidate (Dict[str, Any]): dictionary containing candidate data
+
+    Returns:
+        SQL: SQL update query
+    """
+
+    primary_key = candidate['candidate_id']
+    values = OrderedDict(sorted(candidate.items()))
+
+    query_string = 'UPDATE fec.candidate_detail SET ' \
+        + ', '.join([f' {key}={{}}' for key, val in values.items()])\
+        + ' WHERE candidate_id={}'
+
+    query = sql.SQL(query_string)\
+        .format(*[Literal(val) for key, val in values.items()], Literal(primary_key))
+
+    return query

4ca21e3075f45f72d8190bdedb9b82193b0749d9
Steven Black <genseb7@gmail.com>
2020-07-29 15:24:06 -0400

    add missing FK

--- sql/CommitteeCandidates.sql
+++ sql/CommitteeCandidates.sql
@@ -10,7 +10,8 @@ CREATE TABLE IF NOT EXISTS committeecandidates (
   committee_id VARCHAR(10),
   candidate_id VARCHAR(10),
   PRIMARY KEY(committee_candidate_id),
-  FOREIGN KEY(committee_id) REFERENCES committeedetails(committee_id)
+  FOREIGN KEY(committee_id) REFERENCES committeedetails(committee_id),
+  FOREIGN KEY(candidate_id) REFERENCES candidate_detail(candidate_id)
 ) DISTSTYLE AUTO;
 
 COMMENT ON TABLE committeecandidates is 'Linking table to connect CommitteeDetail to CandidateDetail';

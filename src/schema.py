85a0bf3e7f8d59866a79bb5666b97542992e1097
Steven Black <genseb7@gmail.com>
2020-07-30 11:57:32 -0400

    fix query

--- src/schema.py
+++ src/schema.py
@@ -623,7 +623,7 @@ def f1_supplemental_exists(fec_file_id: str, filing: Dict[str, Any]) -> SQL:
     values = OrderedDict(sorted(filing.items()))
 
     query_string = 'SELECT * FROM fec.form_1_supplemental WHERE ' \
-        + ', '.join([f' {key}={{}}' for key, val in values.items()])
+        + ' AND '.join([f' {key}={{}}' for key, val in values.items()])
 
     query = sql.SQL(query_string)\
         .format(*[Literal(val) for key, val in values.items()])

1bc38597e3d197648869cceb6c6484e73a38ea1e
Steven Black <genseb7@gmail.com>
2020-09-10 12:08:51 -0400

    Revert "wip refactor unruly tests"
    This reverts commit ba5934911b93a71192c9d4c3a2be78d28b6d9472.

---
+++ tests/test_filing_backfill.py
@@ -0,0 +1,17 @@
+#!/bin/env python3
+"""
+tests src/FilingSync.py
+"""
+
+import os
+import pytest
+
+
+event = {}
+context = {}
+
+def test_lambdaBackfillHandler():
+    os.environ['RSS_SNS_TOPIC_ARN'] = 'arn:aws:sns:us-east-1:648881544937:fec-datasync-resources-RSSFeedTopic-176ER9B6NM31K'
+    from src.FilingSync import lambdaBackfillHandler
+    sns_replies = lambdaBackfillHandler(event, context)
+    assert sns_replies

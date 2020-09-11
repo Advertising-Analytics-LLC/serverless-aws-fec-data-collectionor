28adc0e2ef4f982dc15b98ebec13887ce7990fe2
Steven Black <genseb7@gmail.com>
2020-09-11 19:48:42 -0400

    more scripts for working with Qs

--- bin/purge_sqs_qs.py
+++ bin/purge_sqs_qs.py
@@ -1,18 +1,13 @@
 #!/bin/env python3
 
 import boto3
-
+import list_qs
 
 sqs = boto3.client('sqs')
 
-q_name_prefixes = ['candidate-', 'fec-', 'committee-']
 
-flatten = lambda l: [item for sublist in l for item in sublist]
-
-queue_objects = list(map(lambda x: sqs.list_queues(QueueNamePrefix=x), q_name_prefixes))
-queues_nested = list(map(lambda x: x['QueueUrls'], queue_objects))
-queues = flatten(queues_nested)
+queues = list_qs.list_qs()
 q_str = '\t\n'.join(queues)
 print(f'Purging queues:\n{q_str}')
-map(lambda x: sqs.purge_queue(QueueUrl=x), queues)
+nonelist = list(map(lambda x: sqs.purge_queue(QueueUrl=x), queues))
 print('Queues purged')

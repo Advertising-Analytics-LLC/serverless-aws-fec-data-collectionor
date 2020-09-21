4777fd4a277d88b8231e75504804424dfc600881
Steven Black <genseb7@gmail.com>
2020-09-21 12:11:29 -0400

    tidy database code

--- src/database.py
+++ src/database.py
@@ -23,8 +23,7 @@ USERNAME = get_param_value_by_name('/global/fec-schema/username')
 
 
 class Database:
-    """redshift wrapper to handle data serialization
-    """
+    """redshift wrapper to handle data serialization"""
 
     def __init__(self):
         """creates connection to redshift database and performs operations
@@ -33,28 +32,18 @@ class Database:
         self.conn = psycopg2.connect(dbname=DATABASE, user=USERNAME, password=PASSWORD, host=HOSTNAME, port=PORT)
 
     def __enter__(self):
-        """for use as context manager (pythons with statement)
-
-        Returns:
-            self: returns itself
-        """
+        """for use as context manager (pythons with statement)"""
         self.curr = self.conn.cursor()
 
         return self
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
-        """for use as context manager (pythons with statement)
-
-        Args:
-            exc_type ([type]): [description]
-            exc_value ([type]): [description]
-            exc_traceback ([type]): [description]
-        """
+        """for use as context manager (pythons with statement)"""
 
         self.curr.close()
         self.conn.close()
 
-    def _query(self, query: sql.SQL) -> Any:
+    def query(self, query: sql.SQL) -> Any:
         """Query the database and fetch a result.
         If there are no results to fetch the ProgrammingError is caught and None is returned.
 
@@ -69,22 +58,12 @@ class Database:
         try:
             value = self.curr.fetchall()
         except ProgrammingError as err:
+            logger.debug(err)
+            logger.debug(self.conn.notices)
             logger.debug(f'Query had no results, message: {err}')
             return None
 
         return value
-
-    def query(self, query: sql.SQL) -> Any:
-        """exec and commit a single query
-
-        Args:
-            query (sql.SQL): SQL query
-
-        Returns:
-            Any: the result of fetchall
-        """
-        return_value = self._query(query)
-        return return_value
 
     def try_query(self, query: sql.SQL) -> bool:
         """query in a try block. returns bool representing success
@@ -105,22 +84,12 @@ class Database:
             self.rollback()
             return False
 
-    def copy(self, sql, data_file):
-        self.curr.copy_expert(sql, data_file)
-        self.commit()
-
-    def copy_from(self, sql, data_file, sep=','):
-        self.curr.copy_from(data_file, sql, sep=sep)
-        self.commit()
-
     def commit(self):
-        """commits transaction
-        """
+        """commits transaction"""
         self.conn.commit()
 
     def rollback(self):
-        """rolls back transaction
-        """
+        """rolls back transaction"""
         self.conn.rollback()
 
     def record_exists(self, query: sql.SQL) -> bool:
@@ -140,15 +109,7 @@ class Database:
 
 
 def get_insert_query(database_table: str, values_dict: dict) -> psycopg2.sql:
-    """[summary]
-
-    Args:
-        database_table (str): [description]
-        values_dict (dict): [description]
-
-    Returns:
-        SQL: [description]
-    """
+    """ returns an insert query given a DB table and a dictionary of col:vals """
     values = OrderedDict(sorted(values_dict.items()))
 
     query_string = f'INSERT INTO {database_table} ('\

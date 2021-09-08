e2f5ef20e8751c19e9abc859d114727ecdfe21b0
Steven Black <genseb7@gmail.com>
2021-09-08 17:18:22 -0400

    fix committee loader

--- src/CommitteLoader.py
+++ src/CommitteLoader.py
@@ -21,7 +21,7 @@ API_KEY = get_param_value_by_name(os.environ['API_KEY'])
 openFec = OpenFec(API_KEY)
 
 
-def committeLoader(event: dict, context: object):
+def committeLoader(event, context):
     """Gets committee IDs from SQS, pulls data from OpenFEC API, and pushes to RedShift"""
 
     logger.debug(json.dumps(event))
@@ -30,43 +30,44 @@ def committeLoader(event: dict, context: object):
 
     for message in messages:
 
-        committee_id = message
+        committee_id = message['body']
 
         route = f'/committee/{committee_id}/'
-        committee_data = openFec.get_route_paginator(route)
-        for datum in committee_data:
+        committee_paginator = openFec.get_route_paginator(route)
+        for pagination in committee_paginator:
+            for committee_datum in pagination['results']:
+                committee_id = committee_datum['committee_id']
+                candidate_ids = committee_datum.pop('candidate_ids')
 
-            committee_id = datum['committee_id']
-            candidate_ids = committee_data.pop('candidate_ids')
+                for candidate_id in candidate_ids:
+                    with Database() as db:
 
-            for candidate_id in candidate_ids:
+                        record_exists_query = schema.get_committee_candidate_by_id(committee_id, candidate_id)
+                        if db.record_exists(record_exists_query):
+                            continue
+
+                        query = schema.get_committee_candidate_insert_statement(committee_id, candidate_id)
+                        db.try_query(query)
+
+                committee_datum['last_updated'] = "'now'"
+                # rename name -> committee_name
+                committee_datum['committee_name'] = committee_datum.pop('name')
+                # convert cycles list seperated by ~
+                committee_datum = condense_dimension(committee_datum, 'cycles')
+
+                committee_exists_query = schema.get_committee_detail_by_id(committee_id)
+
                 with Database() as db:
 
-                    record_exists_query = schema.get_committee_candidate_by_id(committee_id, candidate_id)
-                    if db.record_exists(record_exists_query):
-                        continue
+                    if db.record_exists(committee_exists_query):
+                        query = schema.get_committee_detail_update_statement(**committee_datum)
+                    else:
+                        query = schema.get_committee_detail_insert_statement(**committee_datum)
 
-                    query = schema.get_committee_candidate_insert_statement(committee_id, candidate_id)
                     db.try_query(query)
 
-            committee_data['last_updated'] = "'now'"
-            # rename name -> committee_name
-            committee_data['committee_name'] = committee_data.pop('name')
-            # convert cycles list seperated by ~
-            committee_data = condense_dimension(committee_data, 'cycles')
-
-            committee_exists_query = schema.get_committee_detail_by_id(committee_id)
-
-            with Database() as db:
-
-                if db.record_exists(committee_exists_query):
-                    query = schema.get_committee_detail_update_statement(**committee_data)
-                else:
-                    query = schema.get_committee_detail_insert_statement(**committee_data)
-
-                db.try_query(query)
-
         route = f'/committee/{committee_id}/filings/?form_category=STATEMENT'
-        committee_filings = openFec.get_route_paginator(route)
-        for filing in committee_filings:
-            upsert_filing(filing)
+        committee_paginator = openFec.get_route_paginator(route)
+        for pagination in committee_paginator:
+            for filing in pagination['results']:
+                upsert_filing(filing)
